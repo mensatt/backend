f8932f4091f1391ee6bf6f03abb82de79e59ac87
StealWonders <6114858+StealWonders@users.noreply.github.com>
2022-05-21 00:28:58 +0200

    non-working changes

---
+++ pkg/utils/utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "os"
+
+func GetDatabaseHost(username string, password string) string {
+	return "postgres://" + username + ":" + password + "@" + os.Getenv("DATABASE_HOST") + "/" + os.Getenv("DATABASE_NAME") + "?sslmode=disable"
+}
