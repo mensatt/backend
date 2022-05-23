06300903ac509d1db75a0b14cb865c05f61aaf62
Tobias Gessler <gessler.tobi@gmail.com>
2022-05-23 22:06:49 +0200

    secure ping

--- internal/misc/main.go
+++ internal/misc/main.go
@@ -4,17 +4,32 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mensatt/mensatt-backend/internal/middleware"
 )
 
 func Run(g *gin.RouterGroup) error {
 	// Simple keep-alive/ping handler
 	g.GET("/ping", pingHandler())
+	g.GET("/secure-ping", securePingHandler())
 	return nil
 }
 
 // pingHandler is simple keep-alive/ping handler
 func pingHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
+	}
+}
+
+// securePingHandler is simple keep-alive/ping handler with auth
+func securePingHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user := middleware.GetUserIDFromCtx(c.Request.Context())
+		if user == nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+
 		c.String(http.StatusOK, "OK")
 	}
 }
