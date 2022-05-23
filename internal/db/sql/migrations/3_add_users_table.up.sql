48e47f8db6c9ca091d9876df8356b9dac779be37
Tobias Gessler <gessler.tobi@gmail.com>
2022-05-23 23:43:55 +0200

    add admin user

--- internal/db/sql/migrations/3_add_users_table.up.sql
+++ internal/db/sql/migrations/3_add_users_table.up.sql
@@ -7,3 +7,5 @@ CREATE TABLE users (
     updated_at timestamptz DEFAULT NOW() NOT NULL,
     PRIMARY KEY (id)
 );
+
+Insert INTO users (email, password_hash) VALUES ('admin@mensatt.de', '$2a$10$pdvY6v8k2McSYbFk3HRDl.h8QfMjOxfpm2CywkDDzfOzlYDZV8NUm');
