3bc46f700d122e7dfe17b7eb6a9049e64305cbea
StealWonders <6114858+StealWonders@users.noreply.github.com>
2022-05-23 23:39:57 +0200

    Adds missing user creation to schema.sql

--- internal/db/sql/schema.sql
+++ internal/db/sql/schema.sql
@@ -80,3 +80,12 @@ CREATE TABLE image (
     PRIMARY KEY (id),
     FOREIGN KEY(occurrence) REFERENCES occurrence(id)
 );
+
+CREATE TABLE users (
+    id uuid DEFAULT uuid_generate_v4(),
+    email varchar UNIQUE NOT NULL,
+    password_hash varchar NOT NULL,
+    created_at timestamptz DEFAULT NOW() NOT NULL,
+    updated_at timestamptz DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (id)
+);
