b6578f658294dc78c8f907c0f415912720fda3e3
StealWonders <6114858+StealWonders@users.noreply.github.com>
2022-05-23 23:39:57 +0200

    Adds missing user creation to schema.sql

--- internal/db/sql/migrations/3_add_users_table.up.sql
+++ internal/db/sql/migrations/3_add_users_table.up.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 CREATE TABLE users (
     id uuid DEFAULT uuid_generate_v4(),
-    email varchar NOT NULL UNIQUE,
+    email varchar UNIQUE NOT NULL,
     password_hash varchar NOT NULL,
     created_at timestamptz DEFAULT NOW() NOT NULL,
     updated_at timestamptz DEFAULT NOW() NOT NULL,
