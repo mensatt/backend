1e51c17def03f2de1193e1edf9fda68bf70bc525
Tobias Gessler <gessler.tobi@gmail.com>
2022-06-20 16:08:59 +0200

    fix typo

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,11 +2,6 @@ package utils
 
 import "os"
 
-func FileOrFirExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil
-}
-
 func CreateDirIfNotExists(dir string) error {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err = os.Mkdir(dir, 0755)
