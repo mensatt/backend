a62cb9dd30ec59c38cca4159424a2681af77640e
StealWonders <6114858+StealWonders@users.noreply.github.com>
2024-02-02 16:18:33 +0100

    removes un-used code

--- internal/graphql/resolvers/helper.go
+++ internal/graphql/resolvers/helper.go
@@ -13,50 +13,6 @@ import (
 	"net/http"
 )
 
-//func (r *mutationResolver) storeImages(tx *ent.Tx, ctx context.Context, review *ent.Review, images []*models.ImageInput) ([]*ent.Image, error) {
-//	var imageEntities []*ent.Image
-//	for _, image := range images {
-//		if image.Image.Size > int64(r.ImageUploader.GetMaxImageSize()) {
-//			return nil, fmt.Errorf("image size is too large") // let the caller handle the transaction rollback
-//		}
-//
-//		imageBytes, err := io.ReadAll(image.Image.File)
-//		if err != nil {
-//			return nil, err // let the caller handle the transaction rollback
-//		}
-//
-//		imageUUID, imageHash, err := r.ImageUploader.ValidateAndStoreImage(imageBytes, image.Rotation)
-//		if err != nil {
-//			return nil, err // let the caller handle the transaction rollback
-//		}
-//
-//		// on tx rollback remove image
-//		tx.OnRollback(func(next ent.Rollbacker) ent.Rollbacker {
-//			return ent.RollbackFunc(func(ctx context.Context, tx *ent.Tx) error {
-//				err := r.ImageUploader.RemoveImage(imageUUID)
-//				if err != nil {
-//					return err
-//				}
-//
-//				return next.Rollback(ctx, tx)
-//			})
-//		})
-//
-//		imageEntity, err := tx.Image.Create().
-//			SetID(imageUUID).
-//			SetImageHash(imageHash).
-//			SetReview(review).
-//			Save(ctx)
-//		if err != nil {
-//			return nil, err // let the caller handle the transaction rollback
-//		}
-//
-//		imageEntities = append(imageEntities, imageEntity)
-//	}
-//
-//	return imageEntities, nil
-//}
-
 func (r *mutationResolver) submitImages(uuids []uuid.UUID) []uuid.UUID {
 	var submittedImages []uuid.UUID
 
